7aaf6c972dab012435d31dcfb9437c75b1689cf8
googoocone1 <googoocone1@naver.com>
2024-11-20 07:33:17 +0900

    add google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,31 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "./head";
+import Script from "next/script";
 
 export const metadata = {
   title: "수원 개인회생 개인파산 전문 법무법인 에이파트",
   description:
-    "수원 개인회생, 수원 개인파산, 수원 개인회생 전문 변호사, 수원 변호사, 수원 회생,",
+    "수원 개인회생 개인파산 전문 법무법인 에이파트 수원 김태용 변호사입니다.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {/* Google Tag Manager (noscript) - Body */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P9LKW4FQ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {children}
-      </body>
+    <html lang="ko">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16722031338"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16722031338');
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
